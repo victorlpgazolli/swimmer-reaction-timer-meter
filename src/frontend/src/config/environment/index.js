c2e3677fda6d0bd2a69503deb437f33812bc7843
victor gazolli <victorlpgazolli@gmail.com>
2021-12-15 21:45:38 -0300

    [ref] websocket host

--- src/frontend/src/config/environment/index.js
+++ src/frontend/src/config/environment/index.js
@@ -1,4 +1,4 @@
 export const BACKEND_HOST = "swimmer-reaction-timer-meter.herokuapp.com"
 
 export const API_URL = "https://" + BACKEND_HOST;
-export const WEBSOCKET_URL = "wss://" + BACKEND_HOST;
+export const WEBSOCKET_URL = "wss://" + BACKEND_HOST + ":443";
