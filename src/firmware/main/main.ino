0191d90c6592f121308bf86e285796613d6cd326
ViniciusZancanari <viniciuszanca@hotmail.com>
2022-01-28 22:17:34 -0300

    add/sensor

---
+++ src/firmware/main/main.ino
@@ -0,0 +1,18 @@
+sensorIR = 13;
+void setup() {
+Serial.begin(9600);
+pinMode (sensorIR, OUTPUT);
+
+}
+
+void loop() {
+
+if (digitalRead(sensorIR) == HIGH)
+{
+ Serial.write("Ok");
+}
+else
+{
+Serial.write("Não está funcionando");
+}
+}
