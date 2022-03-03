b2e91610d10219de0ced1c6ed0c888a983b240cf
ViniciusZancanari <viniciuszanca@hotmail.com>
2022-03-02 23:08:40 -0300

    [add] Wi-FI connection

--- src/firmware/main/main.ino
+++ src/firmware/main/main.ino
@@ -1,18 +1,36 @@
+#include "WiFiEsp.h"
 int sensorIR = 13;
-void setup() {
-Serial.begin(9600);
-pinMode (sensorIR, OUTPUT);
 
+
+// Id e password do Wi-fi
+char ssid[] = "alledraB_2.4G";
+char pass[]= "15072000";
+int status = WL_IDLE_STATUS;
+
+void setup() {
+  Serial.begin(115200);
+
+    // Conectando no WIFI
+
+  while ( status != WL_CONNECTED) {
+    Serial.print("Conecntando no ssid: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("Conectado");
+  
+  pinMode (sensorIR, OUTPUT);
 }
 
 void loop() {
 
-if (digitalRead(sensorIR) == HIGH)
-{
- Serial.write("Ok");
-}
-else
-{
-Serial.write("Não está funcionando");
-}
+  if (digitalRead(sensorIR) == HIGH)
+  {
+   Serial.write("Ok");
+  }
+  else
+  {
+  Serial.write("Não está funcionando");
+  }
 }
