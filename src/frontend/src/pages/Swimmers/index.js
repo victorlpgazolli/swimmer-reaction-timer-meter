8e4bf03eb5ee89f0a9afd0a9b6153319b67d3331
victor gazolli <victorlpgazolli@gmail.com>
2022-03-03 22:01:43 -0300

    [ref] swimmer list components style

--- src/frontend/src/pages/Swimmers/index.js
+++ src/frontend/src/pages/Swimmers/index.js
@@ -35,9 +35,26 @@ function SwimmersPage({
                         backgroundColor: "#fff",
                     }}>
                     <a
+                        style={{
+                            padding: "10px",
+                            background: "#50BBCA",
+                            borderRadius: "30px",
+                            color: "#fff",
+                            textDecoration: "none",
+                            boxShadow: "1px 1px 2px #16363a",
+                            fontSize: "14px",
+                        }}
                         href='#/nadadores/adicionar'
                     > Adicionar </a>
-                    <input placeholder='Procurar nadador' />
+                    <input
+                        style={{
+                            padding: "10px",
+                            margin: "0 10px",
+                            borderRadius: "23px",
+                            borderWidth: "2px",
+                            borderColor: "#50BBCA",
+                        }}
+                        placeholder='Procurar nadador' />
                 </div>
                 <div
                     style={{
@@ -50,20 +67,14 @@ function SwimmersPage({
                     }}>
                     <div style={{
                         padding: 15,
-                        backgroundColor: "#00000020",
+                        backgroundColor: "#8CE6F5",
                         margin: "10px 0px",
                         flex: 1,
                         display: "flex",
                         justifyContent: "space-between",
                         alignItems: "center",
+                        borderRadius: "12px"
                     }}>
-                        <div style={{
-                            padding: "4px 8px",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}>
-                            foto
-                        </div>
                         <div style={{
                             flex: 1,
                             justifyContent: "center",
@@ -76,6 +87,86 @@ function SwimmersPage({
                             alignItems: "center",
                         }}>
                             toggle
+                        </div>
+                    </div>
+                    <div style={{
+                        padding: 15,
+                        backgroundColor: "#0B4C55",
+                        margin: "10px 0px",
+                        flex: 1,
+                        display: "flex",
+                        borderRadius: "12px",
+                        flexDirection: "column"
+                    }}>
+                        <div style={{
+                            flex: 1,
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}>
+                            <div style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center",
+                                color: "#fff"
+                            }}>
+                                nome
+                            </div>
+                            <div style={{
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}>
+                                toggle
+                            </div>
+                        </div>
+                        <div style={{
+                            flex: 1,
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}>
+                            <div style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center",
+                                padding: "20px",
+                                paddingBottom: 0
+                            }}>
+                                <div style={{
+                                    flexDirection: "row",
+                                    display: "flex",
+                                    flex: 1,
+                                    borderWidth: 1,
+                                    borderColor: "#8CE6F5",
+                                    borderRadius: 15
+                                }}>
+                                    <div style={{
+                                        justifyContent: "flex-start",
+                                        alignItems: "center",
+                                        display: "flex",
+                                        flex: 1,
+                                        padding: 8,
+                                    }}>
+                                        Salto 1
+                                    </div>
+                                    <div style={{
+                                        justifyContent: "flex-end",
+                                        alignItems: "center",
+                                        display: "flex",
+                                        flex: 1,
+                                        padding: 8,
+                                    }}>
+                                        00:030
+                                    </div>
+                                </div>
+                                <div style={{
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    display: "flex"
+                                }}>
+                                    Ver mais
+                                </div>
+                            </div>
                         </div>
                     </div>
 
