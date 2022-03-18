3300d7a6781d0a93213a8cbf654b6c33bd089f88
victor gazolli <victorlpgazolli@gmail.com>
2022-03-17 23:01:11 -0300

    [add] feedback to training

--- src/frontend/src/pages/Swimmers/SwimmerTraining.js
+++ src/frontend/src/pages/Swimmers/SwimmerTraining.js
@@ -1,12 +1,10 @@
-import { convertReactionTimeToDuration } from 'helpers';
 import React from 'react'
-import { LoadMore, SwimmerJumpBody, SwimmerJumpContent, SwimmerJumpName, SwimmerJumpTime, SwimmerNameOpen, SwimmerOpen } from './style'
+import { SwimmerJumpBody, SwimmerJumpContent, SwimmerJumpName, SwimmerJumpTime, SwimmerNameOpen, SwimmerOpen } from './style'
 import Toggle from './toggle';
 function SwimmerTraining({
     swimmer,
     onToggle
 }) {
-    console.log(swimmer);
     return (
         <SwimmerOpen>
             <div>
@@ -22,10 +20,6 @@ function SwimmerTraining({
                 <SwimmerJumpBody>
                     {
                         swimmer.trainings.map((training, index) => {
-                            const {
-                                seconds,
-                            } = convertReactionTimeToDuration(training.reaction_time_diff_in_milliseconds);
-                            console.log(training.reaction_time_diff_in_milliseconds);
                             return (
                                 <SwimmerJumpContent>
                                     <SwimmerJumpName >
