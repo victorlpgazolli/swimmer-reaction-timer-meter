05c61bcf7e78b002c602ea690a15c46ba056bed3
victor gazolli <victorlpgazolli@gmail.com>
2022-03-17 22:52:40 -0300

    [fix] env

--- src/frontend/src/config/environment/index.js
+++ src/frontend/src/config/environment/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_HOST = "localhost:8082"
+export const BACKEND_HOST = "swimmer-reaction-timer-meter.herokuapp.com"
 
-export const API_URL = "http://" + BACKEND_HOST + "/api/v1";
-export const WEBSOCKET_URL = "wss://" + BACKEND_HOST + ":8082";
+export const API_URL = "https://" + BACKEND_HOST + "/api/v1";
+export const WEBSOCKET_URL = "wss://" + BACKEND_HOST + ":443";
