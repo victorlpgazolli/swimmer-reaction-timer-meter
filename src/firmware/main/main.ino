65485281629621b66b068064e7d7f2ed3cda34cf
ViniciusZancanari <viniciuszanca@hotmail.com>
2022-04-29 22:55:44 -0300

    [add]botton and sensor time(seconds)

--- src/firmware/main/main.ino
+++ src/firmware/main/main.ino
@@ -1,64 +1,98 @@
 #include <ESP8266WiFi.h>
+//#include <Timer.h>
 
-// Id e password do Wi-fi
+/* 
+* Id e password do Wi-fi 
+*/
 #define ssid "VINICIUS_2G"
 #define password  "vini2014"
 
-int sensorIR    = 15;
-int ledON_OFF   = 14;
-int ledWiFI     = 13;
-int ledSensor   = 12;
+#define botao 16
+#define sensorIR 15
+#define ledON_OFF 14
+#define ledWiFI 13
+#define ledSensor 12
 
-int WiFiConectado = LOW;
-
-//"alledraB_2.4G";
-//"15072000";
-
+int statusBotao;
+int cronometro = 0;
 
 void setup() {
- 
-  Serial.begin(115200);
-  pinMode (sensorIR, OUTPUT);
+  Serial.begin(9600);
+
+  pinMode (botao, INPUT_PULLUP);
+  pinMode (sensorIR, INPUT);
   pinMode (ledON_OFF, OUTPUT);
   pinMode (ledWiFI, OUTPUT);
 
-  //Status do Microcontrolador
+/*
+*Status do Microcontrolador
+*/
+  
   digitalWrite(ledON_OFF, HIGH);
-    
+
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-  
+
   Serial.println("");
   Serial.println("WiFi connected");
-  digitalWrite(ledWiFI,HIGH);
-  
+  digitalWrite(ledWiFI, HIGH);
 }
 
-void loop() {
-  
-  if (digitalRead(WiFi.status() != WL_CONNECTED))
-  {
-    digitalWrite(ledWiFI, LOW); 
-    Serial.println("wifi conetadoo"); 
-  }
- 
+void loop()
+{
+  conecaoWiFi();
+  delay(2000);
+  tempoSensor();
+  exibirTempo();
+}
 
-  //Condição do sensor
-  if (digitalRead(sensorIR) == HIGH)
+
+/*
+ * Métodos
+ */
+void conecaoWiFi()
+{
+   if (digitalRead(WiFi.status() != WL_CONNECTED))
   {
-   Serial.write("O nadador está na plataforma");
-   digitalWrite(ledSensor,HIGH);
+    digitalWrite(ledWiFI, LOW);
   }
-  else
+}
+
+void tempoSensor()
+{
+/*
+ * Sensor
+ * HIGH = Qunado tira o pé da base
+ * LOW = Quando o pé está na base
+ * 
+ */
+ cronometro = 0; 
+ statusBotao = digitalRead(botao);
+  if (statusBotao == LOW )
   {
-  Serial.write("Salto realizado");
-  digitalWrite(ledSensor,LOW);
+    Serial.println("botão apertado");
+    while (digitalRead(sensorIR) ==LOW)
+    {
+      digitalWrite(ledSensor, HIGH);
+      cronometro = cronometro+1;
+      Serial.println(cronometro);
+      delay(1000);
+    }
+    digitalWrite(ledSensor, LOW);
   }
-  
+}
+
+void exibirTempo()
+{
+  Serial.print("Salto realizado em");
+  Serial.print(cronometro);
+  Serial.println("segundos");
+  digitalWrite(ledSensor, LOW);
 }
