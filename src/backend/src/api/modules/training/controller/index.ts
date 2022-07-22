b64804d72763308368f215955e3ef5e807786298
victor gazolli <victorlpgazolli@gmail.com>
2022-07-21 23:23:21 -0300

    [add] try catch

--- src/backend/src/api/modules/training/controller/index.ts
+++ src/backend/src/api/modules/training/controller/index.ts
@@ -3,38 +3,44 @@ import config from "@config";
 import axios from "axios";
 import { assert } from "console";
 const createNewTraining: Endpoint = async (req, res) => {
-    const {
-        reaction_time_diff_in_milliseconds
-    } = req.query
-    assert(
-        String(reaction_time_diff_in_milliseconds).length > 0,
-        "reaction_time_diff_in_millisecond should be > 0"
-    );
+    try {
+        const {
+            reaction_time_diff_in_milliseconds
+        } = req.query
+        assert(
+            String(reaction_time_diff_in_milliseconds).length > 0,
+            "reaction_time_diff_in_millisecond should be > 0"
+        );
 
-    const getCurrentSwimmerUrl = config.apiFullUrl + config.apiBaseURL + "/v1/swimmer/current";
+        const getCurrentSwimmerUrl = config.apiFullUrl + config.apiBaseURL + "/v1/swimmer/current";
 
-    const currentSwimmer: Swimmer | null = await axios.get(getCurrentSwimmerUrl);
+        const currentSwimmer: Swimmer | null = await axios.get(getCurrentSwimmerUrl);
 
-    if (!currentSwimmer) {
-        console.log(`No current swimmer for ${reaction_time_diff_in_milliseconds}, exiting...`);
-        return res.status(404).json({ error: "No current swimmer" })
+        if (!currentSwimmer) {
+            console.log(`No current swimmer for ${reaction_time_diff_in_milliseconds}, exiting...`);
+            return res.status(404).json({ error: "No current swimmer" })
+        }
+        const {
+            id: currentSwimmerId,
+            coachId
+        } = currentSwimmer;
+
+        assert(
+            String(currentSwimmerId).length > 0,
+            "currentSwimmerId should exists"
+        );
+
+        const createNewTrainingUrl = config.apiFullUrl + config.apiBaseURL + `/v1/swimmer/${currentSwimmerId}/trainings`;
+
+        await axios.post(createNewTrainingUrl, {
+            timestamp: new Date().getTime(),
+            reaction_time_diff_in_milliseconds,
+        });
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({ ok: false })
     }
-    const {
-        id: currentSwimmerId,
-        coachId
-    } = currentSwimmer;
-
-    assert(
-        String(currentSwimmerId).length > 0,
-        "currentSwimmerId should exists"
-    );
-
-    const createNewTrainingUrl = config.apiFullUrl + config.apiBaseURL + `/v1/swimmer/${currentSwimmerId}/trainings`;
-
-    await axios.post(createNewTrainingUrl, {
-        timestamp: new Date().getTime(),
-        reaction_time_diff_in_milliseconds,
-    });
 
     res.status(201).json({ ok: true })
 }
